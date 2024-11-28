bd1524cd4612487cdfbc9e5d7e0cff0e3b27cc17
Ryan Ho Kin Mun <ryanhkm5.13@gmail.com>
2024-11-28 19:29:52 +0800

    Update typos

--- attach-mdf.sql
+++ attach-mdf.sql
@@ -1,5 +1,5 @@
--- SQL command for attaching AdventureWorks2022 MDF.
+-- SQL command for attaching AdventureWorks2012 MDF.
 create database AdventureWorks2012
 on (filename = N'/usr/work/AdventureWorksLT2012_Data.mdf')
 for attach_rebuild_log
-go;
+go
